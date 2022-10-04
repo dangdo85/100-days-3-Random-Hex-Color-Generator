9f3095d6977637dcd6390552655a3cede9e3c535
dangdo85 <danghdo@gmail.com>
2022-10-04 15:24:01 -0400

    random color generator with rgb conversion

--- script.js
+++ script.js
@@ -1,10 +1,34 @@
-const number = document.querySelector(".number");
-const btn = document.querySelector(".generate");
+const hex = document.querySelector(".hex")
+const RGB = document.querySelector(".RGB")
+const pageBtn = document.querySelector(".generate-page")
 
-const generateNumber = () => {
-  const maxRange = document.querySelector('.input').value
-  const randomNumber = Math.floor(Math.random() * maxRange + 1);
-  number.innerHTML = randomNumber;
+const generatePageColor = () => {
+  // use toString(16) to include letters
+  // use substring(2, 8) to take first 6 digits that are not the first 0 and decimal(.)
+  const randomColor = Math.random().toString(16).substring(2, 8)
+  document.body.style.backgroundColor = "#" + randomColor
+  hex.innerHTML = "#" + randomColor
+
+    const aRgbHex = randomColor.match(/.{1,2}/g);
+    const aRgb = [
+        parseInt(aRgbHex[0], 16),
+        parseInt(aRgbHex[1], 16),
+        parseInt(aRgbHex[2], 16)
+    ];
+    RGB.innerHTML = aRgb;
+
+// console.log('1502BE'.convertToRGB());
+// console.log('ff0000'.convertToRGB());
+// console.log('f00'.convertToRGB());
 }
 
-btn.addEventListener("click", generateNumber);
+const generateTextColor = () => {
+  const randomColor = Math.random().toString(16).substring(2, 8)
+  document.body.style.backgroundColor = "#" + randomColor
+}
+
+const generateContainerColor = () => {
+  const randomColor = Math.random().toString(16).substring(2, 8)
+  document.body.style.backgroundColor = "#" + randomColor
+}
+pageBtn.addEventListener("click", generatePageColor)
